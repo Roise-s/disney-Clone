7fa1a01c74943421400a79ae998d11a50140601a
Kgaugelo <milkeyroise@gmail.com>
2024-04-20 20:34:21 +0200

    starting with the Footer

---
+++ src/Component/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer

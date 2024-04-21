e1b585af748b5d82732c00eba443a77d2cba0ee7
Kgaugelo <milkeyroise@gmail.com>
2024-04-21 07:16:00 +0200

    done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Main from './Component/main/main';
 import About from './Component/about/About';
+import Watch from './Component/watch/Watch';
 
 function App() {
   return (
     <div className="App">
       <Main/>
       <About/>
+      <Watch/>
     </div>
   );
 }
---
+++ src/Component/watch/Watch.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './watch.css'
+import watch_icon from '../../imgs/1568143-h-423b370e8dfe.webp'
+
+function Watch() {
+  return (
+    <div className='watch'>
+      <h2>Watch the way you want</h2>
+      <p>Discover the world's greatest stories, all in one place</p>
+      <img src={watch_icon} alt=''></img>
+    </div>
+  )
+}
+
+export default Watch

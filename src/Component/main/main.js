975a13e51e4b1da5bfcc6bece8369a311f477cec
Kgaugelo <milkeyroise@gmail.com>
2024-04-23 10:18:17 +0200

    f

--- src/Component/main/main.js
+++ src/Component/main/main.js
@@ -5,11 +5,15 @@ import Disney_icon from '../../imgs/disney-plus-logo.webp'
 function Main() {
   return (
     <div className='mm'>
+      <a onClick={() => {alert("Hey this will take you to the real Website, if it didn't click the button again By Kgaugelo.dev")}} rel='noreferrer' href='https://apps.disneyplus.com/za/onboarding' target='_Blank'>
         <button className='btn'>Log In</button>
+      </a>
         <div className='main'>
             <img src={Disney_icon} alt='Disney icon' />
             <p>Stream brand new Disney+ Originals, blockbusters, binge-worthy series and more</p>
-            <button>Sign Up Now &gt;</button>
+            <a onClick={() => {alert("Hey this will take you to the real Website, if it didn't click the button again By Kgaugelo.dev")}} rel='noreferrer' href='https://apps.disneyplus.com/za/onboarding' target='_Blank'>
+              <button>Sign Up Now &gt;</button>
+            </a>
         </div>
     </div>
   )
